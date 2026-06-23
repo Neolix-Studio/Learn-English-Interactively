8f9ab4b2d9116231fcd3cd6719ae73e0585c6b09
Neolix <theneolix@gmail.com>
2026-06-23 10:56:15 +0200

    fix: safely generate db_config.php using Node.js to escape PHP variables

---
+++ js/write_db_config.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const esc = (val) => {
+  if (!val) return "''";
+  return "'" + val.replace(/\\/g, '\\\\').replace(/'/g, "\\'") + "'";
+};
+
+const content = `<?php
+// Automatically compiled by GitHub Actions
+define('DB_HOST', ${esc(process.env.DB_HOST)});
+define('DB_NAME', ${esc(process.env.DB_NAME)});
+define('DB_USER', ${esc(process.env.DB_USER)});
+define('DB_PASS', ${esc(process.env.DB_PASS)});
+define('MIGRATION_TOKEN', ${esc(process.env.MIGRATION_TOKEN)});
+`;
+
+fs.writeFileSync('db_config.php', content);
+console.log('db_config.php successfully generated.');
